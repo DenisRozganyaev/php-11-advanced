ff25c861bbb101bd54c8c3bbe7a529ab85c6910c
DenisRozganyaev <denis.rozganyaev@gmail.com>
2022-12-22 22:48:12 +0200

    [lesson_12] Finished with Router, start working with Controller, Models

--- Core/Router.php
+++ Core/Router.php
@@ -18,10 +18,9 @@ class Router
     public function add(string $route, array $params = [])
     {
         $route = preg_replace('/\//', '\\/', $route);
-        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
-
+        $route = preg_replace('/\{([a-z_]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
         $route = "/^{$route}$/i";
+
         $this->routes[$route] = $params;
     }
 
@@ -29,16 +28,59 @@ class Router
     {
         $url = trim($url, '/');
         $url = $this->removeQueryVariables($url);
-        d($this->params);
 
         if ($this->match($url)) {
-            dd($this->params);
+            $this->checkRequestMethod();
+            $controller = $this->getController();
+            $action = $this->getAction($controller);
+
+            if ($controller->before($action)) {
+                call_user_func_array([$controller, $action], $this->params);
+                $controller->after($action);
+            } else {
+                dd('error');
+            }
+        }
+    }
+
+    protected function getController(): Controller
+    {
+        $controller = $this->params['controller'] ?? null;
+        if (!class_exists($controller)) {
+            throw new \Exception("Controller '{$controller}' doesn't exists");
+        }
+        unset($this->params['controller']);
+
+        return new $controller;
+    }
+
+    protected function getAction(Controller $controller): string
+    {
+        $action = $this->params['action'] ?? null;
+        if (!method_exists($controller, $action)) {
+            throw new \Exception("Action '{$action}' in '" . $controller::class . "' doesn't exists");
+        }
+        unset($this->params['action']);
+
+        return $action;
+    }
+
+    protected function checkRequestMethod()
+    {
+        if (array_key_exists('method', $this->params)) {
+            $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
+
+            if ($requestMethod !== strtolower($this->params['method'])) {
+                throw new \Exception("Method '". $_SERVER['REQUEST_METHOD'] ."' not allowed for this route");
+            }
+
+            unset($this->params['method']);
         }
     }
 
     protected function match(string $url)
     {
-        foreach($this->routes as $route => $params) {
+        foreach ($this->routes as $route => $params) {
             if (preg_match($route, $url, $matches)) {
                 $this->params = $this->setParams($route, $matches, $params);
                 return true;
